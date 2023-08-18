805ef1cee23ea3ce29b11d6f59edf1da45b3a7be
charlesnyanje <charleswilliam26@gmail.com>
2023-08-18 11:51:56 +0300

    3. List Tables

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,14 +1,5 @@
 -- Write a script that lists all the tables of a database in your MySQL server.
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Check if a database name is provided as an argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <database_name>"
-    exit 1
-fi
-
-# Get the database name from the first argument
-database_name=$1
-
-# MySQL command to list tables in the specified database
-mysql -u your_username -p -e "USE $database_name; SHOW TABLES;"
+USE mysql;
+SHOW TABLES;
