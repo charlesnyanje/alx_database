939f98ccc3aa725814d7dc5e12a05f9363b92e0a
charlesnyanje <charleswilliam26@gmail.com>
2023-08-18 14:32:56 +0300

    4.First table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- Write a query that creates a table called first_table in the database you're connected to.
 
-CREATE TABLE IF NOT EXISTS ${1}.first_table (
+CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
 );
