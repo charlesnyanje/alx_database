eae9d0f8fcad71bda5883fb1a9e2ab6764bbf6a3
charlesnyanje <charleswilliam26@gmail.com>
2023-08-18 12:08:16 +0300

    3. List Tables

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,5 +1,6 @@
 -- Write a script that lists all the tables of a database in your MySQL server.
 #!/usr/bin/env bash
 
-USE mysql;
-SHOW TABLES;
+mysql -hlocalhost -uroot -p --execute="SHOW TABLES;" "$1"
+
+
