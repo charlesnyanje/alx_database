e89171846861c88649da101549b4123b1fd46e4f
charlesnyanje <charleswilliam26@gmail.com>
2023-08-20 18:46:23 +0300

    8. Cities of California

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -6,7 +6,7 @@
 -- The database name will be passed as an argument of the mysql command
 
 
-SELECT cities.id, states.name
+SELECT cities.id, cities.name
 FROM cities, states
 WHERE cities.state_id = states.id AND states.name = 'California'
 ORDER BY cities.id ASC;
