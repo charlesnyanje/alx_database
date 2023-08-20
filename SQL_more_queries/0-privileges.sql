1b1e676a206c147bc7145e86961721bcdd4156f4
charlesnyanje <charleswilliam26@gmail.com>
2023-08-20 17:34:27 +0300

    0. My privileges!

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- Write an sql command that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+-- Write a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
 
 SELECT * FROM mysql.user WHERE user = 'user_0d_1' OR user = 'user_0d_2';
 
